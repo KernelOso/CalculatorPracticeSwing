493d6814274366b13bd09cba430fc38b190fec10
Oscar D Prieto O <oscardpotsu@gmail.com>
2025-03-18 21:08:38 -0500

    Feature(.view.CalculatorView.java): Add window centering function
    Adds the function to center the Java Swing window on the screen. It works by taking half the horizontal and vertical size of the screen and subtracting half the horizontal and vertical size of the window, obtaining the origin point to display the window.

--- src/main/java/org/kerneloso/view/CalculatorView.java
+++ src/main/java/org/kerneloso/view/CalculatorView.java
@@ -1,15 +1,24 @@
 package org.kerneloso.view;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
 import javax.swing.JFrame;
 
 public class CalculatorView extends JFrame {
 
   public CalculatorView() {
 
-    setBounds( 500 , 300 , 800 , 600);
+    int width = 250;
+    int height = 250;
+    setSize(width,height);
     setResizable(false);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     setTitle("Calculator Swing");
+
+    //Set frame in the center of the screen
+    Toolkit localScreen = Toolkit.getDefaultToolkit();
+    Dimension screenDimension = localScreen.getScreenSize();
+    setLocation((screenDimension.width/2 - width/2) , (screenDimension.height/2 - height/2));
 
   }
 
