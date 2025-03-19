6122a59a399baa51c030b77a8017b25ab781d690
Oscar D Prieto O <oscardpotsu@gmail.com>
2025-03-19 18:53:52 -0500

    Fix(view/CalculatorView): Resolve auto-generated code conflicts
    Adjust manual code to properly integrate with Apache NetBeans Swing GUI builder generated code

--- README.md
+++ README.md
@@ -11,3 +11,10 @@ The technologies and techniques used in this project are:
 - JUnit5
 - Java Swing
 
+## Color Pallete
+
+1. #FCF7FF
+2. #C4CAD0
+3. #878C8F
+4. #A4969B
+5. #655560
--- src/main/java/org/kerneloso/view/CalculatorView.java
+++ src/main/java/org/kerneloso/view/CalculatorView.java
@@ -12,6 +12,15 @@ public class CalculatorView extends JFrame {
 
   public CalculatorView() {
 
+    setResizable(false);
+    setTitle("Calculator Swing");
+    int width = 280;
+    int height = 450;
+    setSize(width, height);
+    Toolkit localScreen = Toolkit.getDefaultToolkit();
+    Dimension screenDimension = localScreen.getScreenSize();
+    setLocation((screenDimension.width / 2 - width / 2), (screenDimension.height / 2 - height / 2));
+
     initComponents();
 
   }
@@ -19,32 +28,40 @@ public class CalculatorView extends JFrame {
   // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
   private void initComponents() {
 
-    setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-    setResizable(false);
-    setTitle("Calculator Swing");
+    jPanel_Main = new javax.swing.JPanel();
 
-    int width = 400;
-    int height = 300;
-    setSize(width, height);
-    Toolkit localScreen = Toolkit.getDefaultToolkit();
-    Dimension screenDimension = localScreen.getScreenSize();
-    setLocation((screenDimension.width / 2 - width / 2), (screenDimension.height / 2 - height / 2));
+    setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+    jPanel_Main.setBackground(new java.awt.Color(252, 247, 255));
+
+    javax.swing.GroupLayout jPanel_MainLayout = new javax.swing.GroupLayout(jPanel_Main);
+    jPanel_Main.setLayout(jPanel_MainLayout);
+    jPanel_MainLayout.setHorizontalGroup(
+        jPanel_MainLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 280, Short.MAX_VALUE)
+    );
+    jPanel_MainLayout.setVerticalGroup(
+        jPanel_MainLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 450, Short.MAX_VALUE)
+    );
 
     javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
     getContentPane().setLayout(layout);
     layout.setHorizontalGroup(
         layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(jPanel_Main, javax.swing.GroupLayout.DEFAULT_SIZE,
+                javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
     );
     layout.setVerticalGroup(
         layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addComponent(jPanel_Main, javax.swing.GroupLayout.DEFAULT_SIZE,
+                javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
     );
 
     pack();
-
   }// </editor-fold>//GEN-END:initComponents
 
   // Variables declaration - do not modify//GEN-BEGIN:variables
+  private javax.swing.JPanel jPanel_Main;
   // End of variables declaration//GEN-END:variables
 }
